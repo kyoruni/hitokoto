b1c67eccb86ddb68d3ec1222dad01794fd3d7afb
kyoruni <40832190+kyoruni@users.noreply.github.com>
2025-08-14 01:10:04 +0900

    :wrench: テーマはconfigから読み込む

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
 
-$theme = 'default';
+date_default_timezone_set('Asia/Tokyo');
+
+$config = require __DIR__ . '/../config.php';
+$theme = $config['theme'] ?? 'default';
+
 require __DIR__ . "/../views/themes/{$theme}/home.php";
