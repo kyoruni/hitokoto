4ffefae20f1c2066e8c31fc84774d8d3890752a3
kyoruni <40832190+kyoruni@users.noreply.github.com>
2025-08-14 02:22:48 +0900

    :wrench: storeだと分かりづらいのでpostに変更

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Asia/Tokyo');
 $config = require __DIR__ . '/../config.php';
 $theme = $config['theme'] ?? 'default';
 
-require __DIR__ . '/../core/store.php';
+require __DIR__ . '/../core/post.php';
 $ids = get_recent_paths($config['posts_per_page']);
 $posts = get_posts($ids);
 
