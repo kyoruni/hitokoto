795d146de18ba07182f9c6376db541bca12d9f0f
kyoruni <40832190+kyoruni@users.noreply.github.com>
2025-08-14 18:07:29 +0900

    :robot: 一覧はlist.phpに移動

--- views/themes/default/home.php
+++ views/themes/default/home.php
@@ -1,18 +1,1 @@
-<div class="posts">
-    <?php if (!empty($posts)): ?>
-        <?php foreach ($posts as $post): ?>
-            <article class="post">
-                <div class="post-meta">
-                    投稿日: <a href="/post.php/<?= $post['id'] ?>"><?= date('Y年n月j日 G:i', strtotime($post['created'])) ?></a>
-                </div>
-                <div class="post-content">
-                    <?= $post['body_html'] ?>
-                </div>
-            </article>
-        <?php endforeach; ?>
-    <?php else: ?>
-        <div class="no-posts">
-            まだ記事がありません
-        </div>
-    <?php endif; ?>
-</div>
+<?php include __DIR__ . '/list.php'; ?>
--- views/themes/default/list.php
+++ views/themes/default/list.php
@@ -1,3 +1,18 @@
-<?php
-
-echo 'default list';
+<div class="posts">
+    <?php if (!empty($posts)): ?>
+        <?php foreach ($posts as $post): ?>
+            <article class="post">
+                <div class="post-meta">
+                    投稿日: <a href="/post.php/<?= $post['id'] ?>"><?= date('Y年n月j日 G:i', strtotime($post['created'])) ?></a>
+                </div>
+                <div class="post-content">
+                    <?= $post['body_html'] ?>
+                </div>
+            </article>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <div class="no-posts">
+            まだ記事がありません
+        </div>
+    <?php endif; ?>
+</div>
