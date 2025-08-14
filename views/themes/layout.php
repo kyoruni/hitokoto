eefff456dc1095ac17c44cc3b7bcc96dbda82ee2
kyoruni <40832190+kyoruni@users.noreply.github.com>
2025-08-14 18:22:42 +0900

    :bug: 記事詳細ページでCSSが読み込めていなかった

--- views/themes/layout.php
+++ views/themes/layout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($config['site_name'] ?? 'hitokoto') ?></title>
-    <link rel="stylesheet" href="themes/<?= $config['theme'] ?>/assets/style.css">
+    <link rel="stylesheet" href="/themes/<?= $config['theme'] ?>/assets/style.css">
 </head>
 <body>
     <div class="header">
